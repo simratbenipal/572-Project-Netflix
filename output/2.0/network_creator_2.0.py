def55f209acbde41c124dcb4ef14619b204dd432
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2024-04-03 17:11:01 -0600

    community info, figures

--- output/2.0/network_creator_2.0.py
+++ output/2.0/network_creator_2.0.py
@@ -2,14 +2,21 @@ import pandas as pd
 import networkx as nx
 
 # Read the CSV data into a pandas DataFrame
+# HRef: https://www.w3schools.com/python/pandas/pandas_csv.asp
 df = pd.read_csv('netflix_titles.csv')
 
+# Things to note: Cast and Directors have multiple CSV values
+# Some cast and directors have missing values (null), ignore those
+# Only include Movies
 # Filter rows where type is 'Movie' and director and cast are not empty
+# Href: https://www.geeksforgeeks.org/ways-to-apply-an-if-condition-in-pandas-dataframe/
 movies_df = df[(df['type'] == 'Movie') & (df['director'].notnull()) & (df['cast'].notnull())]
 
 # Create an undirected graph
 G = nx.Graph()
 
+# Iterate over all the rows in CVS, split the director, split the cast
+# add the nodes as movies, and director and cast (if not added before)   
 # Add nodes for movies, directors, and cast
 for index, row in movies_df.iterrows():
     movie = row['title']
@@ -21,6 +28,9 @@ for index, row in movies_df.iterrows():
     for actor in cast:
         G.add_node(actor, type='cast')
 
+# Iterate over the rows in CSV, for every movie split the director and cast, and for each director, add an edge from movie to each director
+# add an edge from movie to each actor
+# add an edge from each actor to each director
 # Add edges between movies and directors/cast
 for index, row in movies_df.iterrows():
     movie = row['title']
@@ -34,4 +44,10 @@ for index, row in movies_df.iterrows():
             G.add_edge(director, actor)
 
 # Export the graph to a GEXF file
+# Href: https://stackoverflow.com/questions/31450090/writing-a-networkx-graph-with-position-color-ect-to-gexf
 nx.write_gexf(G, 'movie_network_undirected.gexf')
+
+# This will make the graph tripartite
+# Edge set #1 : Movie to Director(s)
+# Edge set #2 : Movie to Cast Member(s)
+# Edge set #3 : Cast to Director, if they have worked together
